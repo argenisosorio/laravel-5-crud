54d38c7670d678af2d356f9c02055e7bf707a1db
Argenis Osorio <argenisosorio580@hotmail.com>
2021-05-05 13:20:01 -0400

    Agregados los campos de la tabla en el modelo Student

--- app/Student.php
+++ app/Student.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    protected $table = "articles";
+    protected $fillable = ['first_name','last_name','identification_number'];
 }
